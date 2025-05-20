4ae3d17854e6481b14d57757cd9dd2a0dd8b5c56
Simon Lynch <simon.lynch@hashicorp.com>
2025-05-20 17:09:23 +1000

    Refactor JWT auth backend role configuration by updating token policies and commenting out unused role definitions

--- auth_jwt_tfc.tf
+++ auth_jwt_tfc.tf
@@ -9,8 +9,7 @@ resource "vault_jwt_auth_backend_role" "admins" {
   backend         = vault_auth_backend.jwt_tfc.path
   role_name       = "vault_admins"
   token_policies  = [
-    #vault_policy.generate_tfc_token.name,
-    "default"
+    "admin"
   ]
 
   bound_audiences = [
@@ -30,30 +29,30 @@ resource "vault_jwt_auth_backend_role" "admins" {
   }
 }
 
-resource "vault_jwt_auth_backend_role" "tfc" {
-  backend         = vault_auth_backend.jwt_tfc.path
-  role_name       = "tfc"
-  token_policies  = [
-    "terraform_cloud",
-    "vault_unseal",
-    "generate_certificate",
-    "create_child_token",
-    "sign_ssh_certificate",
-    ]
+# resource "vault_jwt_auth_backend_role" "tfc" {
+#   backend         = vault_auth_backend.jwt_tfc.path
+#   role_name       = "tfc"
+#   token_policies  = [
+#     "terraform_cloud",
+#     "vault_unseal",
+#     "generate_certificate",
+#     "create_child_token",
+#     "sign_ssh_certificate",
+#     ]
 
-  bound_audiences = [
-    "vault.tfc.workspace.identity"
-  ]
-  bound_claims_type = "glob"
-  bound_claims = {
-    sub = "organization:tfo-apj-demos:*"
-    terraform_organization_id = "org-6nfrqkZhPPHJWG5h"
-  }
-  user_claim      = "terraform_full_workspace"
-  role_type       = "jwt"
+#   bound_audiences = [
+#     "vault.tfc.workspace.identity"
+#   ]
+#   bound_claims_type = "glob"
+#   bound_claims = {
+#     sub = "organization:tfo-apj-demos:*"
+#     terraform_organization_id = "org-6nfrqkZhPPHJWG5h"
+#   }
+#   user_claim      = "terraform_full_workspace"
+#   role_type       = "jwt"
 
-  claim_mappings = {
-    terraform_project_id = "terraform_project_id"
-    terraform_workspace_id = "terraform_workspace_id"
-  }
-}
+#   claim_mappings = {
+#     terraform_project_id = "terraform_project_id"
+#     terraform_workspace_id = "terraform_workspace_id"
+#   }
+# }
