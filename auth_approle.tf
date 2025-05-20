e463b8e21b31fa8e3990b99070787896233805b0
Simon Lynch <simon.lynch@hashicorp.com>
2025-05-20 16:42:36 +1000

    Add approle authentication backend configuration for Ansible Automation Platform

---
+++ auth_approle.tf
@@ -0,0 +1,15 @@
+resource "vault_auth_backend" "approle" {
+  type = "approle"
+}
+
+# Set up the approle role for Ansible Automation Platform
+resource "vault_approle_auth_backend_role" "ansible" {
+  backend         = vault_auth_backend.approle.path
+  role_name       = "aap"
+  token_policies  = ["sign_ssh", "update_approle", "read_kv"]
+}
+
+resource "vault_approle_auth_backend_role_secret_id" "id" {
+  backend   = vault_auth_backend.approle.path
+  role_name = vault_approle_auth_backend_role.ansible.role_name
+}
