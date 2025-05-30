af45de38e2164ee6ba67832ac2ba8a3df3e3721e
Simon Lynch <simon.lynch@hashicorp.com>
2025-05-30 15:48:36 +1000

    Add output for AppRole mount accessor in Terraform configuration

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,6 @@
 
+# output approle_mount_accessor
+output "approle_mount_accessor" {
+  description = "Mount accessor for the AppRole authentication backend"
+  value       = vault_auth_backend.approle.accessor
+}
