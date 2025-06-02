2c81b7a988887e6b171682ce2404d3d8e8db69a5
Simon Lynch <simon.lynch@hashicorp.com>
2025-06-02 16:29:03 +1000

    Add kvv2 secrets engine mount configuration and outputs

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,9 @@ output "approle_mount_accessor" {
   description = "Mount accessor for the AppRole authentication backend"
   value       = vault_auth_backend.approle.accessor
 }
+
+# kvv2_mount_path
+output "kvv2_mount_path" {
+  description = "Path for the KV v2 secrets engine"
+  value       = vault_mount.kvv2.path
+}
